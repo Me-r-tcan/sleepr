f38f54aaa654ce0a17b32b9393cc7d9e57f6cdb7
Mertcan bas <mertcanbas35@gmail.com>
2023-07-01 22:23:03 +0300

    add validation and logger

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { LoggerModule } from 'nestjs-pino';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationRepository } from './reservations.repository';
 import {
   ReservationDocument,
@@ -15,7 +14,7 @@ import {
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
-    LoggerModule.forRoot(),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
